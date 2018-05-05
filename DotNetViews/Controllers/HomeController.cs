5e30f663aa9861860a46c9f672e9f1b10e0dfa0f
Peter <peter.prentiss@gmail.com>
2018-05-05 16:11:54 -0500

    Working on HomeController

--- DotNetViews/Controllers/HomeController.cs
+++ DotNetViews/Controllers/HomeController.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
 namespace DotNetViews.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
-        public HomeController()
-        {
+        public ViewResult Index() {
+            ViewBag.Message = "Hello, World";
+            ViewBag.Time = DateTime.Now.ToString("HH:mm:ss");
+            return View("DebugData");
         }
+
+        public ViewResult List() => View();
     }
 }
