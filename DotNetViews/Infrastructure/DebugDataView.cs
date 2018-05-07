9f7c5a1fe2b7b831058556f0393bc370e59da196
Peter <peter.prentiss@gmail.com>
2018-05-07 16:23:49 -0500

    Update DebugDataView

--- DotNetViews/Infrastructure/DebugDataView.cs
+++ DotNetViews/Infrastructure/DebugDataView.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+
 namespace DotNetViews.Infrastructure
 {
-    public class DebugDataView
+    public class DebugDataView : IView
     {
-        public DebugDataView()
-        {
+        public string Path => String.Empty;
+
+        public async Task RenderAsync(ViewContext context) {
+            context.HttpContext.Response.ContentType = "text/plain";
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("--RoutingData--");
+            foreach(var kvp in context.ViewData) {
+                sb.AppendLine($"Key: {kvp.Key}, Value: {kvp.Value}");
+            }
+
+            await context.Writer.WriteAsync(sb.ToString());
         }
     }
 }
