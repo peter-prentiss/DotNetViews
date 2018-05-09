328a29306819a35238b5c063a111c65531399bef
Peter <peter.prentiss@gmail.com>
2018-05-09 15:17:36 -0500

    Working on debugdataviewengine

--- DotNetViews/Infrastructure/DebugDataViewEngine.cs
+++ DotNetViews/Infrastructure/DebugDataViewEngine.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines
+
 namespace DotNetViews.Infrastructure
 {
-    public class DebugDataViewEngine
+    public class DebugDataViewEngine : IViewEngine
     {
-        public DebugDataViewEngine()
+        public ViewEngineResult GetView(string executingFilePath, string viewPath, bool isMainPage)
         {
+            return ViewEngineResult.NotFound(viewPath, new string[] { "(Debug View Engine - Get View" });
         }
     }
 }
