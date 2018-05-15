6104a4b7bd40a0f29e973d0793db4c945cfff9b0
Peter <peter.prentiss@gmail.com>
2018-05-15 15:42:13 -0500

    Update HomeController

--- DotNetViews/Controllers/HomeController.cs
+++ DotNetViews/Controllers/HomeController.cs
@@ -5,11 +5,8 @@ namespace DotNetViews.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index() {
-            ViewBag.Message = "Hello, World";
-            ViewBag.Time = DateTime.Now.ToString("HH:mm:ss");
-            return View("DebugData");
-        }
+        public ViewResult Index() =>
+            View(new string[] { "Apple", "Orange", "Pear" });
 
         public ViewResult List() => View();
     }
