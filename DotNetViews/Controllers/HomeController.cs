497f7dd95a509401840da77bcd219cfedc13e203
Peter <peter.prentiss@gmail.com>
2018-06-03 12:18:10 -0500

    Update HomeController

--- DotNetViews/Controllers/HomeController.cs
+++ DotNetViews/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ namespace DotNetViews.Controllers
     public class HomeController : Controller
     {
         public ViewResult Index() =>
-            View(new string[] { "Apple", "Orange", "Pear" });
+            View("MyView", new string[] { "Apple", "Orange", "Pear" });
 
         public ViewResult List() => View();
     }
