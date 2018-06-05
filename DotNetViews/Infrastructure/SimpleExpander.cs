10b5203eee59e9f14756b8dac2bfac5cc35b94fa
Peter <peter.prentiss@gmail.com>
2018-06-05 12:21:53 -0500

    Adding to SimpleExpander class

--- DotNetViews/Infrastructure/SimpleExpander.cs
+++ DotNetViews/Infrastructure/SimpleExpander.cs
@@ -1,10 +1,22 @@
-﻿using System;
-namespace DotNetViews.Infrastructure
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Razor;
+namespace Views.Infrastructure
 {
-    public class SimpleExpander
+    public class SimpleExpander : IViewLocationExpander
     {
-        public SimpleExpander()
+        public void PopulateValues(ViewLocationExpanderContext context)
         {
+            // do nothing - not required
+        }
+        public IEnumerable<string> ExpandViewLocations(
+        ViewLocationExpanderContext context,
+        IEnumerable<string> viewLocations)
+        {
+            foreach (string location in viewLocations)
+            {
+                yield return location.Replace("Shared", "Common");
+            }
+            yield return "/Views/Legacy/{1}/{0}/View.cshtml";
         }
     }
 }
