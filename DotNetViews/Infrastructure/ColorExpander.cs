5afe75e2cb3dd3bdec858b51bc891a137ce5423d
Peter <peter.prentiss@gmail.com>
2018-06-07 12:27:19 -0500

    Update ColorExpander

--- DotNetViews/Infrastructure/ColorExpander.cs
+++ DotNetViews/Infrastructure/ColorExpander.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Razor;
+
+
 namespace DotNetViews.Infrastructure
 {
-    public class ColorExpander
+    public class ColorExpander : IViewLocationExpander
     {
-        public ColorExpander()
+        private static Dictionary<string, string> Colors
+            = new Dictionary<string, string>
+            {
+                ["red"] = "Red",
+                ["green"] = "Green",
+                ["blue"] = "Blue"
+            };
+        public void PopulateValues(ViewLocationExpanderContext context)
         {
+            var routeValues = context.ActionContext.RouteData.Values;
+            string color;
+            if (routeValues.ContainsKey("id")
+                    && Colors.TryGetValue(routeValues["id"] as string, out color)
+                    && !string.IsNullOrEmpty(color)) {
+                context.Values["color"] = color;
+            }
         }
     }
 }
